9b06b299eeb24bdc9583dd343058678157b3a386
CapitanSgabello <72787682+CapitanSgabello@users.noreply.github.com>
2020-10-26 11:31:34 +0100

    ImpostazioneProgetto
    Sistemazione in cartelle.
    Import dei package usati.
    Creazione Scena castello.

---
+++ P-Project/Assets/Script/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 30f;
+    private Vector2 moveInput;
+    public Joystick joystick;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+        /* Movimenti Del Giocatore */
+        moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
+        Vector3 moveHorizontal = transform.up * -moveInput.x;
+        Vector3 moveVertical = transform.right * moveInput.y;
+        rb.velocity = (moveHorizontal + moveVertical) * speed;
+    }
+
+
+}
