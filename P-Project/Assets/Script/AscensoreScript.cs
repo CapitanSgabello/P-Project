6428878ce3451c2b5f22ac014a227f59279348ed
CapitanSgabello <72787682+CapitanSgabello@users.noreply.github.com>
2020-11-04 00:29:59 +0100

    Sistemazione Assi
    Raddrizzamento Assi 3D: Asse Y Dedicata all'Altezza.
    Aggiunta Oggetto e Script "Ascensore".
    Modifica Script "PlayerController" per adattare il movimento agli assi e far funzionare l'ascensore.
    Aggiunta gravità artificiale al Player.
    Creazione Modello 3D del cubo con due materiali da usare nelle TileMap.
    Aggiunta Collider3D a Muri e Pavimenti.

---
+++ P-Project/Assets/Script/AscensoreScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AscensoreScript : MonoBehaviour
+{
+    public GameObject elevator;
+    public bool stop;
+    public bool startElevator;
+    public float limiteInferiore;
+    public float delay;
+
+    public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        stop = true;
+        startElevator = false;
+        limiteInferiore = elevator.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startElevator)
+        {
+            delay -= Time.deltaTime;
+            if (delay <= 0f)
+            {
+                elevator.transform.Translate((Vector3.up) * speed * Time.deltaTime);
+                PlayerController.instance.isOnElevator(true);
+            }
+        }
+        else
+        {
+            delay -= Time.deltaTime;
+            if(delay <= 0 && !stop)
+            {
+                elevator.transform.Translate((-Vector3.up) * speed * Time.deltaTime);
+                if (elevator.transform.position.y <= limiteInferiore)
+                {
+                    stop = true;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            delay = 0.5f;
+            startElevator = true;
+            stop = false;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController.instance.isOnElevator(false);
+            delay = 1.5f;
+            startElevator = false;
+        }
+    }
+
+
+
+}
--- P-Project/Assets/Script/PlayerController.cs
+++ P-Project/Assets/Script/PlayerController.cs
@@ -21,17 +21,42 @@ public class PlayerController : MonoBehaviour
     public float minAngleVisual = -10f;
     public float maxAngleVisual = 10f;
 
+    /* Variabili e costanti per la gravità */
+    private Vector3 playerFall;
+    public float gravityValue;
+    public float gravity = 800f;
+
+    /* Variabili e costanti per gli ascensori */
+    public Transform groundCheck;
+    public float groundDistance = 1f;
+    public LayerMask elevatorMask;
+    public float elevatorSpeed;
+    public bool isGrounded;
+    public bool onElevator;
+
     private void Start()
     {
-
-        
-     }
+        gravityValue = gravity;
+        elevatorSpeed = 170f;
+    }
     private void Awake()
     {
         instance = this;
     }
     private void Update()
     {
+        /* Impostazioni per l'Ascensore */
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, elevatorMask);
+
+        if (isGrounded && onElevator)
+        {
+            gravityValue = elevatorSpeed;
+        }
+        else
+        {
+            gravityValue = gravity;
+        }
+
         Move();
         LookAround();
     }
@@ -50,10 +75,29 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
+        /*  Vecchi Comandi di movimento
         Vector2 movementDirection = new Vector2(joystickMove.Horizontal, joystickMove.Vertical)* moveSpeed* Time.deltaTime; 
         Vector3 moveHorizontal = transform.up* -movementDirection.x;
         Vector3 moveVertical = transform.right * movementDirection.y;
         characterController.Move(moveHorizontal + moveVertical) ;
+        */
+
+        /* Movimenti Del Giocatore */
+        Vector3 moveInput = new Vector3(joystickMove.Horizontal, 0f, joystickMove.Vertical);
+        Vector3 moveHorizontal = transform.forward * moveInput.z;
+        Vector3 moveVertical = transform.right * moveInput.x;
+        Vector3 move = moveHorizontal + moveVertical;
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+
+        playerFall.y = gravityValue * Time.deltaTime;
+
+        characterController.Move(playerFall * Time.deltaTime);
+    }
+
+    /* Viene richiamata dall'oggetto ascensore che informa il giocatore di essere su un ascensore */
+    public void isOnElevator(bool condition)
+    {
+        onElevator = condition;
     }
 
 }
