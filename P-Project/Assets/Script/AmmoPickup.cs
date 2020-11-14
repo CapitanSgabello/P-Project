5b7f25bb2afbb2c3d6d126a3d56c0c783aeb38cf
marco2298 <marco.dalbis@gmail.com>
2020-11-14 13:58:10 +0100

    Aggiunta di loot e script
    Ho aggiunto cassa di munizioni e medikit, creato gli script per raccogliere gli item e aggiunto altri sprite

---
+++ P-Project/Assets/Script/AmmoPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+
+    public int ammoAmount = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerController.instance.currentAmmo += ammoAmount;
+
+            Destroy(gameObject);
+        }
+    }
+}
