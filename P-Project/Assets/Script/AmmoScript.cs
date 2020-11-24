ef78d6a9d46e1c43f102f2cad303cfc3384432f9
Francesco9932 <francescostefanelli98@gmail.com>
2020-11-24 20:19:31 +0100

    AmmoCounter

---
+++ P-Project/Assets/Script/AmmoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoScript : MonoBehaviour
+{
+    public Text score;
+    public float ammo;
+    void Start()
+    {
+        score = GetComponent<Text>();
+        ammo = PlayerController.instance.currentHealth;
+    }
+
+    void Update()
+    {
+        ammo = PlayerController.instance.currentAmmo;
+        score.text = "" + ammo;
+    }
+}
