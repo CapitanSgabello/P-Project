4994cc67343f03a046aecdc9ec9a2848dac0533c
Francesco9932 <francescostefanelli98@gmail.com>
2020-11-30 19:56:40 +0100

    LoadingScene e Restyling
    implementata la scena di caricamento e restyle menu impostazioni

---
+++ P-Project/Assets/Script/SceneLoading.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour
+{
+    public Image progressBar;
+    private void Start()
+    {
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+    private IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gamelevel = SceneManager.LoadSceneAsync(2);
+        while (gamelevel.progress<=1)
+        {
+            progressBar.fillAmount = gamelevel.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
