6f64cab5f27afc4dafac38dea98d0b29fbd6a1f9
Francesco9932 <francescostefanelli98@gmail.com>
2021-01-08 10:48:24 +0100

    Firebase database asset import

---
+++ P-Project/Assets/Script/SavingData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Firebase;
+using Firebase.Database;
+
+public class SavingData : MonoBehaviour
+{
+    void Start()
+    {
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+}
