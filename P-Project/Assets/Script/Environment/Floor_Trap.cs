95048386aa416c0457e5342d8a8c7262a0295383
CapitanSgabello <72787682+CapitanSgabello@users.noreply.github.com>
2021-02-13 13:15:02 +0100

    Modifiche secondo Livello
    Aggiunta di Munizioni e decorazioni nel secondo livello e modifica della struttura.
    Creazione del pavimento di lava (manca texture).
    Risoluzione problemi Sprite della chiave.
    Creazione dei prefabs per le decorazioni.

---
+++ P-Project/Assets/Script/Environment/Floor_Trap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor_Trap : MonoBehaviour
+{
+
+    // public Collider areoOfDamage;
+    private int dmg_amount = 1;
+    private float rateOfDmg = 0.15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rateOfDmg > 0)
+        {
+            rateOfDmg -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && rateOfDmg <= 0)
+        {
+            PlayerController.instance.TakeDamage(dmg_amount);
+            rateOfDmg = 0.15f;
+        }
+    }
+}
